102e44b878328787512a34a6cd07a993d87151b5
Thangmai02 <maivanthang10a6@gmail.com>
2024-02-22 19:03:40 +0700

    up

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,21 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        float a= scanner.nextFloat();
-        float b= scanner.nextFloat();
-        System.out.println("tong la"+(a+b));
+        System.out.println("20 số nguyên tố đầu tiên là:");
+        int dem=0;
+        for(int i=2;dem<20;i++){
+            boolean soNT =true;
+            for (int j=2;j<=Math.sqrt(i);j++){
+                if(i%j==0){
+                    soNT=false;
+                    break;
+                }
+            }
+            if(soNT){
+                System.out.println(i+"");
+                dem++;
+            }
+        }
+
     }
-}
+    }
--- src/donguyento.java
+++ src/donguyento.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class donguyento {
+public class donguyento {
+    public static void main(String[] args) {
+        System.out.println("20 số nguyên tố đầu tiên là:");
+        int dem=0;
+        for(int i=2;dem<20;i++){
+            boolean soNT =true;
+            for (int j=2;j<=Math.sqrt(i);j++){
+                if(i%j==0){
+                    soNT=false;
+                    break;
+                }
+            }
+            if(soNT){
+                System.out.println(i+"");
+                dem++;
+            }
+        }
+
+    }
 }
